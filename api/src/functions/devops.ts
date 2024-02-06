725957c698db48b794e85d6a3a556b4de8b99306
Vinny Martins <martins.vds@outlook.com>
2024-02-05 23:55:22 -0700

    Add dependencies and update CSS

--- api/src/functions/devops.ts
+++ api/src/functions/devops.ts
@@ -1,5 +1,40 @@
-import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
+import { app, input, output, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 import { AzureDevOpsApi } from "../apis/devops/azureDevops";
+import { randomUUID } from "crypto";
+import { TableClient } from "@azure/data-tables";
+
+const tableClient = TableClient.fromConnectionString(getTableConnectionString(), 'AzureDevOpsProjectRequests');
+
+const tableInput = input.table({
+    tableName: 'AzureDevOpsProjectRequests',
+    connection: 'TableConnectionString'
+});
+
+const tableOutput = output.table({
+    tableName: 'AzureDevOpsProjectRequests',
+    connection: 'TableConnectionString'
+});
+
+const queueOutput = output.storageQueue({
+    queueName: 'new-requests-queue',
+    connection: 'TableConnectionString',
+});
+
+interface ProjectRequest {
+    PartitionKey: string;
+    RowKey: string;
+    id: string;
+    organization: string;
+    project: string;
+    processTemplateId: string;
+    url: string;
+    status: "Pending" | "Approved" | "Rejected" | "Failed";
+    createdAt: Date;
+}
+
+function getTableConnectionString(): string {
+    return process.env["TableConnectionString"];
+}
 
 async function getOrganizations(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     return {
@@ -9,11 +44,81 @@ async function getOrganizations(request: HttpRequest, context: InvocationContext
 
 async function getProcessTemplates(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     const org = request.params.organization;
-    
+
     return {
         jsonBody: await AzureDevOpsApi.getProcessTemplates(org)
     };
 };
+
+async function requestNewProject(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    const org = request.params.organization;
+    const payload = (await request.json() as any);
+    const project = payload.projectName;
+    const processId = payload.processId;
+
+    const id = randomUUID();
+
+    const projectRequest = {
+        PartitionKey: org,
+        RowKey: id,
+        id: id,
+        organization: org,
+        project: project,
+        processTemplateId: processId,
+        status: 'Pending',
+        url: '',
+        createdAt: new Date()
+    } as ProjectRequest;
+
+    context.extraOutputs.set(tableOutput, projectRequest)
+    context.extraOutputs.set(queueOutput, projectRequest);
+
+    return {
+        status: 201
+    };
+}
+
+async function projectRequests(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    const entries = <ProjectRequest[]>(await context.extraInputs.get(tableInput));
+
+    const requests = entries.map((entry: ProjectRequest) => {
+        const { PartitionKey, RowKey, ...rest } = entry;
+        return rest;
+    });
+
+    return {
+        jsonBody: requests
+    };
+}
+
+async function updateProjectRequest(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    const org = request.params.organization;
+    const requestId = request.params.requestId;
+    const updated = <ProjectRequest>(await request.json());
+
+    tableInput.filter = `partitionKey eq '${org}' and rowKey eq '${requestId}'`;
+    tableInput.take = 1;
+
+    const projectRequest = <ProjectRequest>(await context.extraInputs.get(tableInput))[0];
+
+    if (projectRequest) {
+        const newStatus = updated.status;
+
+        const { PartitionKey, RowKey, ...existing } = projectRequest;
+
+        tableClient.updateEntity({
+            partitionKey: projectRequest.PartitionKey,
+            rowKey: projectRequest.RowKey,
+            ...existing,
+            ...updated
+        }, "Merge");
+    }
+
+    return {
+        status: 204
+    };
+
+}
 
 async function createProject(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     const org = request.params.organization;
@@ -23,7 +128,7 @@ async function createProject(request: HttpRequest, context: InvocationContext): 
 
     return {
         jsonBody: await AzureDevOpsApi.createProject(org, projectName, processId),
-        status: 201    
+        status: 201
     };
 };
 
@@ -47,6 +152,27 @@ app.http('processes', {
     methods: ['GET'],
     route: 'organizations/{organization}/processes',
     handler: getProcessTemplates
+});
+
+app.http('requestProject', {
+    methods: ['POST'],
+    route: 'organizations/{organization}/project-requests',
+    handler: requestNewProject,
+    extraOutputs: [tableOutput, queueOutput],
+});
+
+app.http('projectRequests', {
+    methods: ['GET'],
+    route: 'organizations/{organization}/project-requests',
+    handler: projectRequests,
+    extraInputs: [tableInput],
+});
+
+app.http('updateRequestProject', {
+    methods: ['PATCH'],
+    route: 'organizations/{organization}/project-requests/{requestId}',
+    handler: updateProjectRequest,
+    extraInputs: [tableInput]
 });
 
 app.http('newProject', {
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,33 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { Box, Container, CssBaseline, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [organization, setOrganization] = useState('')
+  const [organizations, setOrganizations] = useState<string[]>([]) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+  const [process, setProcess] = useState('')
+  const [processes, setProcesses] = useState<{
+    id: string
+    name: string
+  }[]>([])
+
+  const handleOrganizationChange = (event: SelectChangeEvent) => {
+    setOrganization(event.target.value as string)
+  }
+
+  const handleProcessChange = (event: SelectChangeEvent) => {
+    setProcess(event.target.value as string)
+  }
+
+  useEffect(() => {
+    const fetchOrganizations = async () => {
+      try {
+        const response = await fetch('http://localhost:7071/api/organizations')
+        const orgs = await response.json()
+        setOrganizations(orgs)
+      } catch {
+        console.error('Error fetching organizations')
+        setOrganizations([])
+      }
+    }
+
+    fetchOrganizations();
+  }, [])
+
+  useEffect(() => {
+    if (organization === '') return
+
+    const fetchProcessess = async () => {
+      try {
+        const response = await fetch(`http://localhost:7071/api/organizations/${organization}/processes`)
+        const processes = await response.json()
+
+        setProcesses(processes)
+      } catch {
+        console.error('Error fetching processes')
+      }
+    }
+
+    fetchProcessess()
+  }, [organization])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CssBaseline />
+      <Container maxWidth={false} sx={{
+        width: '50%',
+      }}>
+        <Box sx={{ backgroundColor: "white", height: '100vh', width: '100%' }}>
+          <FormControl sx={{
+            width: '200px',
+          }}>
+            <InputLabel id="organization-label">Organization</InputLabel>
+            <Select
+              labelId="organization"
+              id="organization-select"
+              value={organization}
+              label="Organization"
+              onChange={handleOrganizationChange}
+            >
+              {organizations.map((organization) => {
+                return (
+                  <MenuItem value={organization}>
+                    {organization}
+                  </MenuItem>
+                )
+              })}
+            </Select>
+          </FormControl>
+          <FormControl sx={{
+            width: '200px',
+            marginTop: '20px',
+          }}>
+            <InputLabel id="process-label">Process Template</InputLabel>
+            <Select
+              labelId="process-label"
+              id="process-select"
+              value={process}
+              label="Process Template"
+              onChange={handleProcessChange}
+            >
+              {processes.map((process) => {
+                return (
+                  <MenuItem value={process.id}>
+                    {process.name}
+                  </MenuItem>
+                )
+              })}
+            </Select>
+          </FormControl>
+          <TableContainer sx={{
+            marginTop: '20px',
+          }}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Organization</TableCell>
+                  <TableCell align="right">Project</TableCell>
+                  <TableCell align="right">Link</TableCell>
+                  <TableCell align="right">Status</TableCell>
+                </TableRow>
+                <TableBody>
+                </TableBody>
+              </TableHead>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Container>
     </>
   )
 }
